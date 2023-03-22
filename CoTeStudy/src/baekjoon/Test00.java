2f2d0bb873b4cdce07fc72052aaa6cfd93b3e710
zhyunk <gimwlgus@gmail.com>
2023-03-23 01:49:29 +0900

    23.03.23 백준 : 공백으로 붙은 문자열에 콤마 붙여주는 출력문 붙임

--- CoTeStudy/src/baekjoon/Test00.java
+++ CoTeStudy/src/baekjoon/Test00.java
@@ -6,6 +6,9 @@ public class Test00 {
     public Test00(int launcher) throws IOException {
         if (launcher == 1)  this.main();
         else                this.test();
+
+//        배열 만들기
+//        System.out.println("2 5 1 4 3".replace(" ", ","));
     }
 
     public void main() throws IOException {
