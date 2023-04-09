36d6de0998db940821d8c61d2001b632795d0d43
zhyunk <gimwlgus@gmail.com>
2023-04-09 15:58:15 +0900

    23.04.09 백준 _ 실행 파일 출력 값 정리 🤓

--- CoTeStudy/src/baekjoon/Main.java
+++ CoTeStudy/src/baekjoon/Main.java
@@ -1,13 +1,11 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int[][] arr = new int[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())];
+            int n = Integer.parseInt(br.readLine());
 
         } catch (Exception e) {
             System.out.println("err 😭");
